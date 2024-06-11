fc03e788ea66082959543e3f9c93a7b14f379389
JuanPabloFloresDiaz <20220599@ricaldone.edu.sv>
2024-06-11 01:56:19 -0600

    Diseño 90% del Inicio de sesión

--- src/navigation/BottomTab.js
+++ src/navigation/BottomTab.js
@@ -1,7 +1,7 @@
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, Text, StyleSheet } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
-
-// Pantallas de navegación
 import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 
@@ -15,22 +15,117 @@ export default function BottomTab({ logueado, setLogueado }) {
   );
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Inicio') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Equipos') {
+            iconName = focused ? 'football' : 'football-outline';
+          } else if (route.name === 'Perfil') {
+            iconName = focused ? 'person' : 'person-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#FFC300',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: styles.tabBar,
+        headerStyle: {
+          backgroundColor: '#FFC300',
+        },
+        headerTintColor: '#fff',
+        tabBarLabelStyle: styles.tabBarLabel,
+      })}
+    >
       <Tab.Screen
-        name="HomeScreen"
+        name="Equipos"
+        component={LoginScreen}
+        options={{
+          title: 'Equipos',
+        }}
+      />
+      <Tab.Screen
+        name="Inicio"
         component={renderHomeScreen}
         options={{
           title: 'Inicio',
-          tabBarActiveTintColor: '#FFC300', // Color activo de la pestaña
-          headerStyle: {
-            backgroundColor: '#FFC300', // Color del header
-          },
-          headerTintColor: '#fff', // Color del texto en el header
-          tabBarIcon: ({ color }) => ( // Función que define el ícono de la pestaña
-            <Ionicons name="home" color={color} size={24} /> // `color` proviene de React Navigation
+          tabBarButton: props => (
+            <CustomTabBarButton {...props} />
           ),
+        }}
+      />
+      <Tab.Screen
+        name="Perfil"
+        component={LoginScreen}
+        options={{
+          title: 'Perfil',
         }}
       />
     </Tab.Navigator>
   );
 }
+
+const CustomTabBarButton = ({ children, onPress }) => (
+  <View style={styles.customButtonContainer}>
+    <View style={styles.shadow}>
+      <Ionicons name="home" size={30} color="#fff" style={styles.customButtonIcon} />
+    </View>
+    <View style={styles.customButton}>
+      <TouchableOpacity
+        style={styles.customButtonTouchable}
+        onPress={onPress}
+      >
+        {children}
+      </TouchableOpacity>
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  tabBar: {
+    position: 'absolute',
+    bottom: 25,
+    left: 20,
+    right: 20,
+    elevation: 0,
+    backgroundColor: '#000080',
+    borderRadius: 15,
+    height: 90,
+  },
+  tabBarLabel: {
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+  customButtonContainer: {
+    top: -30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  customButton: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: '#000080',
+  },
+  customButtonTouchable: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  customButtonIcon: {
+    color: '#FFC300',
+  },
+  shadow: {
+    shadowColor: '#7F5DF0',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+});
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,57 +1,115 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Alert} from 'react-native';
-import { TextInput, Button, Text } from 'react-native-paper';
+import { View, StyleSheet, ImageBackground, TouchableOpacity, Text, Image } from 'react-native';
+import { TextInput, DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { LinearGradient } from 'expo-linear-gradient';
 import fetchData from '../../api/components';
 
+// Importa la imagen de fondo y el logo
+import background from '../../assets/background.png';
+import logo from '../../assets/gol_blanco 2.png'; 
+
+// Define un tema personalizado
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: 'white',
+    primary: 'white',
+    placeholder: 'white',
+    background: 'transparent',
+  },
+};
+
 const LoginScreen = () => {
-
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Bienvenido</Text>
-      <TextInput
-        label="Usuario"
-        style={styles.input}
-        keyboardType="email-address"
-        autoCapitalize="none"
-      />
-      <TextInput
-        label="Contraseña"
-        style={styles.input}
-        secureTextEntry
-      />
-      <Button mode="contained" style={styles.button}>
-        Iniciar Sesión
-      </Button>
-      <Button mode="contained" style={styles.button}>
-        Cerrar Sesión
-      </Button>
-    </View>
+    <PaperProvider theme={theme}>
+      <ImageBackground source={background} style={styles.background}>
+        <View style={styles.overlay}>
+          <Image source={logo} style={styles.logo} />
+          <TextInput
+            label="Usuario"
+            style={styles.input}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            theme={{ colors: { text: 'white', primary: 'white', placeholder: 'white' } }}
+            underlineColor="transparent"
+            mode="flat"
+            selectionColor="white"
+            placeholderTextColor="white"
+            inputStyle={{ color: 'white' }}  // Asegura que el texto escrito sea blanco
+          />
+          <TextInput
+            label="Clave"
+            style={styles.input}
+            secureTextEntry
+            theme={{ colors: { text: 'white', primary: 'white', placeholder: 'white' } }}
+            underlineColor="transparent"
+            mode="flat"
+            selectionColor="white"
+            placeholderTextColor="white"
+            inputStyle={{ color: 'white' }}  // Asegura que el texto escrito sea blanco
+          />
+          <TouchableOpacity style={styles.button}>
+            <LinearGradient
+              colors={['#354AC8', '#1A2462']}
+              style={styles.gradient}
+            >
+              <Text style={styles.buttonText}>Iniciar sesión</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+          <Text style={styles.forgotPassword}>¿Desea recuperar contraseña?</Text>
+        </View>
+      </ImageBackground>
+    </PaperProvider>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  overlay: {
     flex: 1,
     justifyContent: 'center',
     padding: 20,
-    backgroundColor: '#ffffff',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Overlay con transparencia
   },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
+  logo: {
+    width: 150, // Ajusta el tamaño del logo según tus necesidades
+    height: 150,
+    alignSelf: 'center',
     marginBottom: 20,
-    textAlign: 'center',
-    color: '#000000',
   },
   input: {
     marginBottom: 15,
-    backgroundColor: 'white',
+    backgroundColor: 'transparent', // Hace el fondo del input transparente
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'white',
   },
   button: {
     marginTop: 10,
-    padding: 10,
-    backgroundColor: 'red',
+    alignItems: 'center',
+    borderRadius: 5,
+    overflow: 'hidden',
+  },
+  gradient: {
+    padding: 15,
+    alignItems: 'center',
+    borderRadius: 5,
+    width: '100%',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  forgotPassword: {
+    marginTop: 10,
+    textAlign: 'center',
+    color: '#FFFFFF',
+    textDecorationLine: 'underline',
   },
 });
 

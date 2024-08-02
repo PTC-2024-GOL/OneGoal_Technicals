0f41dad51754b650b022f923e8b3f94e69b3ffe4
sochiiii <20220018@ricaldone.edu.sv>
2024-08-01 23:58:18 -0600

    Perfil de técnico realizado, hermoso, funcional, perfecto

--- api/constantes.js
+++ api/constantes.js
@@ -1,3 +1,3 @@
-export const ip = `192.168.1.6`; 
+export const ip = `192.168.0.13`; 
 export const SERVER_URL = `http://${ip}/sitio_gol_sv/api/`;
 export const IMAGES_URL = `http://${ip}/sitio_gol_sv/api/images/`;

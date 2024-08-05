b801e62b88c4a8386299fb81d59f7260d536adef
Torugam <joelomarmena@gmail.com>
2024-08-05 01:11:49 -0600

    recuperacion de contraseña hecha

--- src/screens/changePasswordsScreen.js
+++ src/screens/changePasswordsScreen.js
@@ -1,16 +1,64 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native'; // Importa useNavigation
+import fetchData from '../../api/components';
+import AlertComponent from '../components/AlertComponent';
 
  
 const RecoverPasswordScreen = ({ logueado, setLogueado }) => {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [response, setResponse] = useState(false);
+  const [alertVisible, setAlertVisible] = useState(false);
+  const [alertType, setAlertType] = useState(1);
+  const [alertMessage, setAlertMessage] = useState('');
+  const [alertCallback, setAlertCallback] = useState(null);
+  const API = 'services/recuperacion/recuperacion.php';
   
   const navigation = useNavigation(); // Obtiene el objeto de navegación
 
-  const handleRecoverPassword = () => {
-    setMessage('¡Revisa tu bandeja de entrada!');
+  const handleRecoverPassword = async () => {
+    if (email === '') {
+        setAlertType(3);
+        setAlertMessage(`El campo no puede estar vacio`);
+        setAlertCallback(null);
+        setAlertVisible(true);
+      return;
+    }
+
+    const fechaActualUTC = new Date();
+    const form = new FormData();
+    form.append('correo', email);
+    form.append('nivel', 2);
+    form.append('fecha', fechaActualUTC.toISOString());
+
+    console.log('Esto contiene el correo: ' + email);
+    console.log('Esto contiene la fecha actual en UTC: ' + fechaActualUTC.toISOString());
+
+    try {
+      const data = await fetchData(API, 'envioCorreo', form);
+      if (data.status) {
+        setAlertType(1);
+        setAlertMessage(`${data.message}`);
+        setAlertCallback(null);
+        setAlertVisible(true);
+      } else {
+        setAlertType(2);
+        setAlertMessage(`${data.error}`);
+        setAlertCallback(null);
+        setAlertVisible(true);
+      }
+    } catch (error) {
+      setAlertType(2);
+      setAlertMessage(`Error: ${error}`);
+      setAlertCallback(null);
+      setAlertVisible(true);
+    }
+  };
+
+  const handleAlertClose = () => {
+    setAlertVisible(false);
+    if (alertCallback) alertCallback();
   };
  
   
@@ -24,18 +72,24 @@ const RecoverPasswordScreen = ({ logueado, setLogueado }) => {
       <TouchableOpacity style={styles.backButton} onPress={handleForgotLogin}>
         <Text style={styles.backText}>←</Text>
       </TouchableOpacity>
-      <Image source={require('../../assets/recuperacion.png')} style={styles.image} />
+      <Image source={require('../../assets/password.png')} style={styles.image} />
       <Text style={styles.label}>Ingresa tu correo electrónico</Text>
       <TextInput
         style={styles.input}
         placeholder="Correo electrónico"
+        keyboardType="email-address"
         value={email}
         onChangeText={setEmail}
       />
-      {message ? <Text style={styles.message}>{message}</Text> : null}
       <TouchableOpacity style={styles.button} onPress={handleRecoverPassword}>
         <Text style={styles.buttonText}>Recuperar contraseña</Text>
       </TouchableOpacity>
+      <AlertComponent
+        visible={alertVisible}
+        type={alertType}
+        message={alertMessage}
+        onClose={handleAlertClose}
+      />
     </View>
   );
 }
